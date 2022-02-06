002f12c0b1a59f2a09b30c09244aaa397bb9e353
Scope.h <hongsch07@naver.com>
2022-02-06 16:35:35 +0900

    [UPDATE] Created Slider, modified Color Button, added Footnote
    - Created Slider, with no accent Color(`.clear`)
    - Now you can mixing color using slider
    - Modified Color Button
    	- backgroundColor syncs with mixed Color.
    	- text foreGround is now toggled white/black when you mix Color.
    - Added simple footnote of instruction in app screen.

--- ColorMixer.swiftpm/ContentView.swift
+++ ColorMixer.swiftpm/ContentView.swift
@@ -1,23 +1,71 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var redValue: Double = 0
+    @State var greenValue: Double = 0
+    @State var blueValue: Double = 0
     var body: some View {
-        ColorView()
-            .frame(maxWidth: 200, maxHeight: 120)
+        VStack{
+            ColorView(red: $redValue, green: $greenValue, blue: $blueValue)
+                .frame(maxWidth: 300, maxHeight: 120)
+                .padding()
+            ColorSlider(value: $redValue, color: .red)
+                .padding(.horizontal)
+            ColorSlider(value: $greenValue, color: .green)
+                .padding(.horizontal)
+            ColorSlider(value: $blueValue, color: .blue)
+                .padding(.horizontal)
+            Text("Slide slider / Input value(0~255) \ninto text field, and mix your color.\n\nTap button to copy hex code into clipboard.")
+                .font(.caption)
+                .foregroundColor(.secondary)
+                .padding()
+                .padding(.top, 120)
+        }
     }
 }
 
 
 struct ColorView: View {
+    @Binding var red: Double
+    @Binding var green: Double
+    @Binding var blue: Double
+    @State var hextriplet: String = "#000000"
     var body: some View {
         Button(action: {}) {
-            Text("#123456")
+            Text(hextriplet)
+                .foregroundColor(textColorInvert())
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .background(Color.red)
+                .background(Color.init(red: red, green: green, blue: blue))
                 .cornerRadius(10)
                 .font(.custom("Menlo", size: 25))
                 .monospacedDigit()
         }
         .accentColor(.white)
+    }
+    
+    func textColorInvert() -> Color {
+        let lightGreen = green > 0.7
+        let lightPink = !lightGreen && red > 0.8 && blue > 0.6
+        if lightGreen || lightPink {
+            return Color.black
+        } else {
+            return Color.white
+        }
+    }
+}
+
+struct ColorSlider: View {
+    @Binding var value: Double
+    @State var color: Color
+    var body: some View {
+        Slider(value: $value, in: 0...1)
+            .accentColor(.clear)
+            .background(color.opacity(value))
+            .cornerRadius(100)
+            .overlay(
+                RoundedRectangle(cornerRadius: 100)
+                    .stroke(.tertiary, lineWidth: 1.5)
+            )
+            .padding()
     }
 }
